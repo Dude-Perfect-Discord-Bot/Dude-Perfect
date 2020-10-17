f44c654865b533b1bc97cf640493ef33b4ca1d2e
Arindam Hazra <arindamhazra987@gmail.com>
2020-10-17 12:45:00 +0530

    Update guilds.js

--- src/commands/Owner Only/guilds.js
+++ src/commands/Owner Only/guilds.js
@@ -14,7 +14,7 @@ class Guilds extends Command {
 
         message.channel.send("** <a:star4:761157512614772736> __My Guilds__ **");
 
-        this.client.guilds.cache.forEach(x => message.channel.send(`<:check:753484699237613630> ${x.name} - \`${x.id}\``))
+        this.client.guilds.cache.forEach(x => message.channel.send(`<:check:753484699237613630> **${x.name}** - \`${x.id}\``))
 
             message.delete();
     }
