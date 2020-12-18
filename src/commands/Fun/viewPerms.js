64be9def8b63eb9e31fb2a955f668c13ef17b1b6
Dhruvin-Purohit <73269605+Dhruvin-Purohit@users.noreply.github.com>
2020-12-18 16:44:50 +0530

    Fixed Grammatical erorr and shortened code

--- src/commands/Fun/viewPerms.js
+++ src/commands/Fun/viewPerms.js
@@ -16,7 +16,7 @@ const { Command } = require('discord-akairo');
 const { MessageEmbed } = require('discord.js');
 
 
-class ViewPerm extends Command {
+module.exports = class ViewPerm extends Command {
     constructor() {
         super('viewPerm', {
             aliases: ['viewperm', 'myperm', 'perm'],
@@ -45,7 +45,7 @@ class ViewPerm extends Command {
             .setTitle(`${member.user.tag}`)
             .setThumbnail(message.guild.iconURL({ dynamic: true }))
             .setColor('#00ff9e')
-            .setDescription(`You have this permissions...${permissions.join(' | ')}`)
+            .setDescription(`You have these permissions...${permissions.join(' | ')}`)
             .setFooter(`Thanks for using ${this.client.user.username}`)
             .setTimestamp();
         message.channel.send(embed);
@@ -62,5 +62,3 @@ class ViewPerm extends Command {
       }
 
 }
-
-module.exports = ViewPerm;
