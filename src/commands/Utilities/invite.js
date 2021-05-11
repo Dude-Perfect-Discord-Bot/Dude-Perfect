2338c618008aca17e4ab31eefcdd5ec8c1442a3a
DeadShot <dvdranz120@gmail.com>
2021-05-11 20:48:12 +0530

    Added Embed in Support & Invite

--- src/commands/Utilities/invite.js
+++ src/commands/Utilities/invite.js
@@ -1,3 +1,4 @@
+const { MessageEmbed }= require('discord.js');
 const { Command } = require('discord-akairo');
 
 class Invite extends Command {
@@ -13,8 +14,15 @@ class Invite extends Command {
     }
 
     exec(message) {
+        const embed = MessageEmbed()
+        .setColor('#00ff9e')
+        .setTitle('Want To Invite Me?')
+        .addField('Invite Link ❯', [
+            '[Add Me Now!](https://discord.com/api/oauth2/authorize?client_id=759763855680602122&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.com%2Finvite%2FZzbZpdw&scope=bot)'
+        ])
+        .setFooter(`${this.client.user.username} is made with ❤️`, `https://cdn.discordapp.com/emojis/805614116937007165.png?v=1`)
 
-        message.reply('**Invite me ❯ ** \nhttps://discord.com/api/oauth2/authorize?client_id=759763855680602122&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.com%2Finvite%2FZzbZpdw&scope=bot')
+        message.channel.send(embed)
 
     }
 }
