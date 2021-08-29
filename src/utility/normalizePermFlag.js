4b4f89a6a415bc23de5203d869ea0bba468e2371
Arindam Hazra <workforxynox@gmail.com>
2021-08-29 21:37:44 +0530

    chore(*): delete files

--- src/utility/normalizePermFlag.js
+++
@@ -1,9 +0,0 @@
-module.exports.normalizePermFlag = (perms) => {
-  return perms.map((perm) =>
-    perm
-      .toLowerCase()
-      .replace(/(^|"|_)(\S)/g, (str) => str.toUpperCase())
-      .replace(/_/g, ' ')
-      .replace(/Use Vad/g, 'Use Voice Acitvity')
-  );
-};
