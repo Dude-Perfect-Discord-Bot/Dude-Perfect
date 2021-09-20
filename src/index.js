3b1c2d06ea9fe71db285047831ba78c00d453460
Arindam Hazra <workforxynox@gmail.com>
2021-09-20 19:24:50 +0530

    fix: unused code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-require('./lib/setup'); 
-const { join } = require('path');
+require('./lib/setup');
 const { Intents: { FLAGS } } = require('discord.js');
 const { LogLevel, SapphireClient } = require('@sapphire/framework');
 
@@ -7,30 +6,33 @@ const { config } = require('dotenv');
 config();
 
 const client = new SapphireClient({
-    shards: 'auto',
-    defaultPrefix: process.env.PREFIX,
-    caseInsensitiveCommands: true,
-    baseUserDirectory: join(__dirname, 'pieces'),
-    intents: Object.values(FLAGS),
-    logger: { level: LogLevel.Debug },
-    presence: {
-        activity: {
-            name: 'dp!help | Made with ❤️',
-            type: 'WATCHING'
-        }
-    }
+	shards: 'auto',
+	defaultPrefix: process.env.PREFIX,
+	caseInsensitiveCommands: true,
+	intents: Object.values(FLAGS),
+	logger: { 
+		level: LogLevel.Debug 
+	},
+	presence: {
+		activities: [
+			{
+				name: 'dp!help | Made with ❤️',
+				type: "WATCHING",
+			},
+		],
+	}
 });
 
 const main = async () => {
-    try {
-        client.logger.info('Logging in . . .');
-        await client.login(process.env.TOKEN);
-        client.logger.info(`DudePerfect is now online!`);
-    } catch (error) {
-        client.logger.fatal(error);
-        client.destroy();
-        process.exit(1);
-    }
+	try {
+		client.logger.info('Logging in');
+		await client.login(process.env.TOKEN);
+		client.logger.info('logged in');
+	} catch (error) {
+		client.logger.fatal(error);
+		client.destroy();
+		process.exit(1);
+	}
 };
 
 main();
