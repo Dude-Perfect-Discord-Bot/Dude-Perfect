2c0644ebe0dab4b6d47bb9e84812117d72f9e397
Arindam Hazra <workforxynox@gmail.com>
2021-09-20 19:22:21 +0530

    feat: add via example

---
+++ src/commands/General/ping.js
@@ -0,0 +1,23 @@
+const { Command } = require('@sapphire/framework');
+const { send } = require('@sapphire/plugin-editable-commands');
+
+class UserCommand extends Command {
+	constructor(context, options) {
+		super(context, {
+			...options,
+			description: 'ping pong'
+		});
+	}
+
+	async run(message) {
+		const msg = await send(message, 'Ping?');
+
+		const content = `Pong from JavaScript! Bot Latency ${Math.round(this.container.client.ws.ping)}ms. API Latency ${
+			msg.createdTimestamp - message.createdTimestamp
+		}ms.`;
+
+		return send(message, content);
+	}
+}
+
+exports.UserCommand = UserCommand;
